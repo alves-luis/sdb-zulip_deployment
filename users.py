72601dbfedfa1fba6f387e988f569ae9a64101a3
hpereira98 <pereirapalmeirahenrique@gmail.com>
2019-12-29 11:16:28 +0000

    csv fix : user not sending private msgs to himself

--- users.py
+++ users.py
@@ -7,7 +7,10 @@ num_users = int(sys.argv[1])
 row_list = [["EMAIL","PASSWORD","PRIVATE_TO"]]
 
 for i in range(num_users):
-    row = ["user"+str(i+1)+"@email.com","exemplo","user"+str(random.randint(1,num_users))+"@email.com"]
+    randNum = random.randint(1,num_users)
+    while randNum == i:
+        randNum = random.randint(1,num_users)
+    row = ["user"+str(i+1)+"@email.com","exemplo","user"+str(randNum)+"@email.com"]
     row_list.append(row)
 
 with open(str(num_users)+'users.csv', 'w', newline='') as file:
